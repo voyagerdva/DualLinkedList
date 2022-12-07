9c9922985c4177ee7e4bf94e66bcdf020e489547
voyagerdva <mariner-9@yandex.ru>
2022-12-07 16:58:23 +0300

    Initial commit

---
+++ src/main/java/ru/zhuravl/DualLinkList.java
@@ -0,0 +1,10 @@
+package ru.zhuravl;
+
+public class DualLinkList {
+    void createNewLinkedList() {
+        System.out.println("--------------");
+        System.out.println("From DualLinkedList class - we created LL !!!");
+        System.out.println("--------------");
+    }
+
+}
