43c69ddc8f85bd0bf2733216f83da5541bfc7e54
voyagerdva <mariner-9@yandex.ru>
2022-12-09 14:40:49 +0300

    modify intersect()

--- src/main/java/ru/zhuravl/DualLinkList.java
+++ src/main/java/ru/zhuravl/DualLinkList.java
@@ -1,10 +1,78 @@
 package ru.zhuravl;
 
-public class DualLinkList {
-    void createNewLinkedList() {
-        System.out.println("--------------");
-        System.out.println("From DualLinkedList class - we created LL !!!");
-        System.out.println("--------------");
+public class DualLinkList extends LinkList implements InterfaceDualList {
+    class DualNode extends LinkList.Node implements InterfaceDualItem {
+
+        DualLinkList.DualNode previous;
+
+        DualNode(String value, DualNode next, DualNode previous) {
+            super(value, next);
+            this.previous = previous;
+        }
+
+        @Override
+        public InterfaceDualItem getPrevious() {
+            return previous;
+        }
+
     }
+
+    @Override
+    public void add(String data) {
+        super.add(data);
+    }
+
+    @Override
+    protected InterfaceItem createNode(String data) {
+        if (super.getTail() == null)
+            return new DualLinkList.DualNode(data, (DualNode) null, (DualNode) null);
+        else
+            return new DualLinkList.DualNode(data, (DualNode) null, (DualNode) getHead());
+    }
+
+    @Override
+    public InterfaceDualList intersect(DualLinkList dualLinkList) {
+        DualLinkList list3 = (DualLinkList) super.intersect(dualLinkList);
+        return list3;
+    }
+
+//    @Override
+    protected InterfaceDualList createList() {
+        return new DualLinkList();
+    }
+
+
+    @Override
+    public InterfaceDualList getSubList(int position, int number) {
+        InterfaceDualList list = new DualLinkList();
+
+        InterfaceItem ref = this.getTail();
+
+
+        return list;
+    }
+
+    @Override
+    public void printDualList() {
+        InterfaceDualItem ref = (InterfaceDualItem) getTail();
+        for (int i = 0; i < getSize(); i++) {
+            System.out.printf("%-25s %-35s\n", "getValue():", ref.getValue());
+            System.out.printf("%-25s %-35s\n", "Node:", ref);
+            System.out.printf("%-25s %-35s\n", "getPrevious():", ref.getPrevious());
+            System.out.printf("%-25s %-35s\n", "getNext():", ref.getNext());
+            System.out.println("-------------");
+            ref = (InterfaceDualItem) ref.getNext();
+
+        }
+
+    }
+
+
+
+
+
+
+
+
 
 }
---
+++ src/main/java/ru/zhuravl/InterfaceDualList.java
@@ -0,0 +1,16 @@
+package ru.zhuravl;
+
+public interface InterfaceDualList extends InterfaceList {
+
+//    InterfaceDualList intersect(DualLinkList dualLinkList);
+
+//    InterfaceDualList intersect(DualLinkList dualLinkList);
+
+    InterfaceDualList intersect(DualLinkList dualLinkList);
+
+    InterfaceDualList getSubList(int position, int number);
+    void printDualList();
+
+
+
+}
